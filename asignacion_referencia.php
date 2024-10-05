df5c140b3c55e2013823781e22aa2ab7e1fb446c
edwin villabona <edwinvillabona0701@gmail.com>
2024-10-05 11:10:37 -0500

    fin

--- asignacion_referencia.php
+++ asignacion_referencia.php
@@ -1,2 +1,16 @@
 <?php
 
+$texto = "fundamento de programacion en PHP";
+
+$asignacion = $texto;
+
+$referencia = &$texto;
+
+echo $asignacion;
+echo "<chr>";
+echo $referencia;
+echo $texto = "asignacion por referencia";
+echo "<chr>";
+echo $referencia;
+echo "<chr>";
+echo $asignacion;
